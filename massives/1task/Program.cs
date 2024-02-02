b55aa220b860ef3f9c6a2dde612accc3e80693af
Dmitrii_Serebriakov <serebryakovdk@gmail.com>
2024-02-02 15:32:35 +0300

    massives

---
+++ massives/1task/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int[5];
+arr[0] = 4;
+arr[1] = 1;
+arr[2] = 9;
+arr[3] = 8;
+arr[4] = 3;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+
+int[] arr2 = new int[5](1, 2, 3, 4, 5);
+
+
+int[] arr3 = (5, 6, 7, 8, 9);
