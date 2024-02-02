64ed99f8c8f95a7d79380f94022ca458ca520e34
Dmitrii_Serebriakov <serebryakovdk@gmail.com>
2024-02-02 17:40:05 +0300

    сделал практику с лекции о массивах

---
+++ massives/4task/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = 10;
+int[] array = { 2, 3, 4, 7, 8, 1, 12, 15, 22, 6 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine(max);
+
+//   j++; ===  j=j+1;
+// нашли наибольший элемент массива
