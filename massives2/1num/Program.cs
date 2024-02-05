402ee1987ece8d416afed4620b4f554e4ea1255e
Dmitrii_Serebriakov <serebryakovdk@gmail.com>
2024-02-05 09:33:12 +0300

    сделали ручное создание массива вводом его размера и элементов

---
+++ massives2/1num/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Введите размер массива ");
+
+// int.Parse преобразует строчное значение в конкретное число
+// строка 12345 становится числом 12 тысяч 345
+
+int size = int.Parse(Console.ReadLine()!);
+
+int[] numbers = new int[size];
+
+System.Console.WriteLine($"Введите {size} элементов массива:");
+for (int i = 0; i < size; i++)
+{
+    int input = int.Parse(Console.ReadLine()!);
+    numbers[i] = input;
+}
+
+System.Console.WriteLine("массив: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    System.Console.Write(numbers[i] + " ");
+}
