35ad3838c8c7ed47377df7d4ea234e20bf7de91d
Dmitrii_Serebriakov <serebryakovdk@gmail.com>
2024-02-06 13:01:08 +0300

    completed 5task

---
+++ functions/4task/Program.cs
@@ -0,0 +1,20 @@
+﻿//Random rnd = new Random();
+
+int size = 10;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    //  arr_int[i] = rnd.Next(1, 100);
+    System.Console.WriteLine("введите целочисленный элемент массива: ");
+    string input = Console.ReadLine()!;
+    arr_int[i]= Convert.ToInt32(input);
+    i++;
+}
+
+i = 0;
+while (i < size)
+{
+    System.Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}
